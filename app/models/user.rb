38ad92815195f33f64f19c41bded1db9bcc4fc99
teshio0512 <teshio0512@gmail.com>
2024-08-25 15:24:23 +0900

    チャットルームの削除機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :room_users
   has_many :rooms, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 
